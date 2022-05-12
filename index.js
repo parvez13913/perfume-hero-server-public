3ec0f79037e7cffc8813ea1dd0c2ab08782fd9e0
parvez13913 <parvezz13913@gmail.com>
2022-05-12 21:23:29 +0600

    single aitem api done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 
@@ -18,12 +18,19 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("perfumeHero").collection("service");
-        app.get('/services', async (req, res) => {
+        app.get('/inventory', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
+
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const inventory = await serviceCollection.findOne(query);
+            res.send(inventory);
+        })
     }
 
     finally {
