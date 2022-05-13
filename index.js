dca662fbe46ff1291e1d5aa1d496a39bfd1a3c2e
parvez13913 <parvezz13913@gmail.com>
2022-05-13 17:55:05 +0600

    delete api complite

--- index.js
+++ index.js
@@ -38,6 +38,15 @@ async function run() {
             const result = inventoryCollection.insertOne(newInventory);
             res.send(result);
         });
+
+        // DELETE
+
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = inventoryCollection.deleteOne(query);
+            res.send(result);
+        })
     }
 
     finally {
