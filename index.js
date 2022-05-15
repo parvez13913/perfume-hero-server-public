77641d0e75ca4f002b87e2bc91fcb2c17effeefa
parvez13913 <parvezz13913@gmail.com>
2022-05-16 03:45:36 +0600

    Simple JWT Implemented

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
+const { status } = require('express/lib/response');
 require('dotenv').config();
 const app = express();
 
@@ -10,7 +12,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    next();
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
 
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ massage: 'Forbidden Access' });
+        }
+
+        req.decoded = decoded;
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pptkl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -73,12 +90,19 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/myInventory', async (req, res) => {
+        app.get('/myInventory', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email
-            const query = { email: email };
-            const cursor = myInventoryCollection.find(query);
-            const myInventory = await cursor.toArray(cursor);
-            res.send(myInventory);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = myInventoryCollection.find(query);
+                const myInventory = await cursor.toArray(cursor);
+                res.send(myInventory);
+            }
+
+            else {
+                res.status(403).send({ massage: 'Forbidden Access' });
+            }
         });
         app.delete('/myInventory/:id', async (req, res) => {
             const id = req.params.id;
@@ -86,6 +110,14 @@ async function run() {
             const result = await myInventoryCollection.deleteOne(query);
             res.send(result);
         });
+
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
     }
 
     finally {
